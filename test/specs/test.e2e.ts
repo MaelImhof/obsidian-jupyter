effcb79abe83bac837fbed9d628e1ebcc240e7b0
Maël Imhof <contact@mael.im>
2025-06-16 18:24:23 +0200

    test: end-to-end test runs lmao how
    I will wait until I get it properly working with the right typing and all, but it's already incredible, all thanks to jesse-r-s-hines on GitHub.

---
+++ test/specs/test.e2e.ts
@@ -0,0 +1,14 @@
+import { browser } from '@wdio/globals'
+
+describe('Test my plugin', function() {
+    before(async function() {
+        // You can create test vaults and open them with reloadObsidian
+        // Alternatively if all your tests use the same vault, you can
+        // set the default vault in the wdio.conf.ts.
+        await browser.reloadObsidian({vault: "./test-vault"});
+    })
+    it('test command open-sample-modal-simple', async () => {
+        const jupyterButton = await browser.$('[aria-label="Create Jupyter Notebook"]');
+        await expect(jupyterButton).toExist();
+    })
+})
