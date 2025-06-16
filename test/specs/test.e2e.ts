70e28838ecf9af0c5603f64497f131f68e0654a1
Maël Imhof <contact@mael.im>
2025-06-16 18:55:19 +0200

    test: remove unused vault reload

--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -5,12 +5,6 @@ import { ObsidianBrowserCommands } from 'wdio-obsidian-service';
 declare const browser: Browser & ObsidianBrowserCommands;
 
 describe('Test my plugin', function() {
-    before(async function() {
-        // You can create test vaults and open them with reloadObsidian
-        // Alternatively if all your tests use the same vault, you can
-        // set the default vault in the wdio.conf.ts.
-        await browser.reloadObsidian({vault: "./test-vault"});
-    })
     it('test command open-sample-modal-simple', async () => {
         const jupyterButton = await browser.$('[aria-label="Create Jupyter Notebook"]');
         await expect(jupyterButton).toExist();
