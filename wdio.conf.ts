eda15884c4856c84e4426fe19741d3597605a113
Maël Imhof <contact@mael.im>
2025-06-17 14:15:55 +0200

    test: allow for more Obsidian instances to run

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -11,7 +11,7 @@ export const config: WebdriverIO.Config = {
     ],
 
     // How many instances of Obsidian should be launched in parallel
-    maxInstances: 1,
+    maxInstances: 8,
 
     capabilities: [{
         browserName: 'obsidian',
