52e4a32b78222817de766f680a402b4bb9f64b24
Maël Imhof <contact@mael.im>
2025-07-16 16:37:03 +0200

    fix: limit to 1 concurrent test in WebDriverIO

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -10,6 +10,9 @@ export const config: WebdriverIO.Config = {
         './test/specs/**/*.e2e.ts'
     ],
 
+    // How many instances of Obsidian should be launched in parallel
+    maxInstances: 1,
+
     capabilities: [{
         browserName: 'obsidian',
         // obsidian app version to download
