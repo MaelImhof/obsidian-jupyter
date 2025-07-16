bdedbda6023ae1d6873260c8157ed329bf19ec03
Maël Imhof <contact@mael.im>
2025-07-16 22:50:33 +0200

    Revert "test: i'm just trying things out at this point"
    This reverts commit bdd7ab4f0a0ac517148a97f199dceff66d232833.

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -11,7 +11,7 @@ export const config: WebdriverIO.Config = {
     ],
 
     // How many instances of Obsidian should be launched in parallel
-    maxInstances: 8,
+    maxInstances: 1,
 
     capabilities: [{
         browserName: 'obsidian',
@@ -37,7 +37,7 @@ export const config: WebdriverIO.Config = {
 
     mochaOpts: {
         ui: 'bdd',
-        timeout: 120000,
+        timeout: 240000,
         // You can set mocha settings like "retry" and "bail"
     },
 
