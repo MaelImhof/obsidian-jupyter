f1ed5d69f7a8f44ee8bc139f870fab99c68b3380
Maël Imhof <contact@mael.im>
2025-07-16 14:52:06 +0200

    chore: setup prettier (#118)
    * chore: setup prettier
    
    * chore: format all files with prettier

--- src/features/create-notebooks/create-notebooks-settings.ts
+++ src/features/create-notebooks/create-notebooks-settings.ts
@@ -1,6 +1,6 @@
-import JupyterForObsidian from "@/jupyter-for-obsidian";
-import { JupyterSettingsTab, SettingsSection } from "@/settings";
-import { DropdownComponent, Setting, ToggleComponent } from "obsidian";
+import JupyterForObsidian from '@/jupyter-for-obsidian';
+import { JupyterSettingsTab, SettingsSection } from '@/settings';
+import { DropdownComponent, Setting, ToggleComponent } from 'obsidian';
 
 /**
  * Lists the ways a newly created notebook can be opened right after its
@@ -12,129 +12,137 @@ import { DropdownComponent, Setting, ToggleComponent } from "obsidian";
  * - opening it in a new window.
  */
 export enum OpenCreatedNotebook {
-    /** The notebook is created but not opened at all. */
-    DONT = "dont-open",
+	/** The notebook is created but not opened at all. */
+	DONT = 'dont-open',
 
-    /**
-     * The notebook is opened in the current tab. This is the default
-     * behavior in vanilla Obsidian. It will replace the currently active
-     * document, or open a new tab if the currently active document
-     * is pinned.
-     */
-    CURRENT_TAB = "current-tab",
+	/**
+	 * The notebook is opened in the current tab. This is the default
+	 * behavior in vanilla Obsidian. It will replace the currently active
+	 * document, or open a new tab if the currently active document
+	 * is pinned.
+	 */
+	CURRENT_TAB = 'current-tab',
 
-    /**
-     * The notebook will be opened in a new tab, whether the current tab
-     * is pinned or not.
-     */
-    NEW_TAB = "new-tab",
+	/**
+	 * The notebook will be opened in a new tab, whether the current tab
+	 * is pinned or not.
+	 */
+	NEW_TAB = 'new-tab',
 
-    /**
-     * The notebook will be opened in a new split tab, which will
-     * be created next to the current tab.
-     */
-    SPLIT = "split",
+	/**
+	 * The notebook will be opened in a new split tab, which will
+	 * be created next to the current tab.
+	 */
+	SPLIT = 'split',
 
-    /** The notebook will be opened in a new window. */
-    WINDOW = "detached-window"
+	/** The notebook will be opened in a new window. */
+	WINDOW = 'detached-window'
 }
 
 /**
  * Settings used by the Create Notebooks feature.
  */
 export interface CreateNotebooksSettings {
-    /**
-     * Whether to display a ribbon icon to create a new Jupyter notebook.
-     * 
-     * This is purely for customization purposes, allowing the user to
-     * hide the ribbon icon if they do not want it to be displayed.
-     */
-    displayFileRibbonIcon: boolean;
+	/**
+	 * Whether to display a ribbon icon to create a new Jupyter notebook.
+	 *
+	 * This is purely for customization purposes, allowing the user to
+	 * hide the ribbon icon if they do not want it to be displayed.
+	 */
+	displayFileRibbonIcon: boolean;
 
-    /**
-     * Whether to display a context menu item to create a new Jupyter
-     * notebook when right-clicking on a folder in the file explorer.
-     */
-    displayFolderContextMenuItem: boolean;
+	/**
+	 * Whether to display a context menu item to create a new Jupyter
+	 * notebook when right-clicking on a folder in the file explorer.
+	 */
+	displayFolderContextMenuItem: boolean;
 
-    /**
-     * The mode in which a newly created notebook will be opened.
-     * 
-     * See {@link OpenCreatedNotebook} for the list of possible
-     */
-    openCreatedFileMode: OpenCreatedNotebook;
+	/**
+	 * The mode in which a newly created notebook will be opened.
+	 *
+	 * See {@link OpenCreatedNotebook} for the list of possible
+	 */
+	openCreatedFileMode: OpenCreatedNotebook;
 }
 
 /**
  * Default setting values for the Create Notebooks feature.
  */
 export const DEFAULT_CREATE_NOTEBOOKS_SETTINGS: CreateNotebooksSettings = {
-    displayFileRibbonIcon: true,
-    displayFolderContextMenuItem: true,
-    openCreatedFileMode: OpenCreatedNotebook.CURRENT_TAB
+	displayFileRibbonIcon: true,
+	displayFolderContextMenuItem: true,
+	openCreatedFileMode: OpenCreatedNotebook.CURRENT_TAB
 };
 
 /**
  * Responsible for registering the settings UI elements for this feature.
- * 
+ *
  * @param tab The settings tab to register the settings UI elements into.
  */
 export function registerCreateNotebooksSettingsUI(tab: JupyterSettingsTab): void {
-    
-    /*=====================================================*/
+	/*=====================================================*/
 	/* Plugin customization settings                       */
 	/*=====================================================*/
 
-    tab.registerSetting({
-        section: SettingsSection.PLUGIN_CUSTOMIZATION,
-        display: (el: HTMLElement, plugin: JupyterForObsidian) => {
-            new Setting(el)
-                .setName("Ribbon icon for new notebooks")
-                .setDesc("Whether to display a ribbon icon that creates a blank Jupyter notebook when clicked.")
-                .addToggle((toggle: ToggleComponent) =>
-                    toggle
-                        .setValue(plugin.settings.displayFileRibbonIcon)
-                        .onChange((value: boolean) => {
-                            plugin.settings.displayFileRibbonIcon = value;
-                        })
-                );
-        }
-    });
+	tab.registerSetting({
+		section: SettingsSection.PLUGIN_CUSTOMIZATION,
+		display: (el: HTMLElement, plugin: JupyterForObsidian) => {
+			new Setting(el)
+				.setName('Ribbon icon for new notebooks')
+				.setDesc(
+					'Whether to display a ribbon icon that creates a blank Jupyter notebook when clicked.'
+				)
+				.addToggle((toggle: ToggleComponent) =>
+					toggle
+						.setValue(plugin.settings.displayFileRibbonIcon)
+						.onChange((value: boolean) => {
+							plugin.settings.displayFileRibbonIcon = value;
+						})
+				);
+		}
+	});
 
-    tab.registerSetting({
-        section: SettingsSection.PLUGIN_CUSTOMIZATION,
-        display: (el: HTMLElement, plugin: JupyterForObsidian) => {
-            new Setting(el)
-                .setName("Folder context menu for new notebooks")
-                .setDesc("If enabled, when you right-click on a folder, one of the actions will be to create a new Jupyter notebook in that folder.")
-                .addToggle((toggle: ToggleComponent) =>
-                    toggle
-                        .setValue(plugin.settings.displayFolderContextMenuItem)
-                        .onChange((value: boolean) => {
-                            plugin.settings.displayFolderContextMenuItem = value;
-                        })
-                );
-        }
-    });
+	tab.registerSetting({
+		section: SettingsSection.PLUGIN_CUSTOMIZATION,
+		display: (el: HTMLElement, plugin: JupyterForObsidian) => {
+			new Setting(el)
+				.setName('Folder context menu for new notebooks')
+				.setDesc(
+					'If enabled, when you right-click on a folder, one of the actions will be to create a new Jupyter notebook in that folder.'
+				)
+				.addToggle((toggle: ToggleComponent) =>
+					toggle
+						.setValue(plugin.settings.displayFolderContextMenuItem)
+						.onChange((value: boolean) => {
+							plugin.settings.displayFolderContextMenuItem = value;
+						})
+				);
+		}
+	});
 
-    tab.registerSetting({
-        section: SettingsSection.PLUGIN_CUSTOMIZATION,
-        display: (el: HTMLElement, plugin: JupyterForObsidian) => {
-            new Setting(el)
-                .setName("Open created notebooks")
-                .setDesc("Whether to open a notebook directly when it is created, and how to open it.")
-                .addDropdown((dropdown: DropdownComponent) => {
-                    dropdown
-                        .addOption(OpenCreatedNotebook.DONT, "Do not open")
-                        .addOption(OpenCreatedNotebook.CURRENT_TAB, "Open in the current tab (default)")
-                        .addOption(OpenCreatedNotebook.NEW_TAB, "Open in a new tab")
-                        .addOption(OpenCreatedNotebook.SPLIT, "Open in a new split tab")
-                        .addOption(OpenCreatedNotebook.WINDOW, "Open in a detached window")
-                        .setValue(plugin.settings.openCreatedFileMode)
-                        .onChange((value: OpenCreatedNotebook) => {
-                            plugin.settings.openCreatedFileMode = value;
-                        });
-                });
-        }
-    });
+	tab.registerSetting({
+		section: SettingsSection.PLUGIN_CUSTOMIZATION,
+		display: (el: HTMLElement, plugin: JupyterForObsidian) => {
+			new Setting(el)
+				.setName('Open created notebooks')
+				.setDesc(
+					'Whether to open a notebook directly when it is created, and how to open it.'
+				)
+				.addDropdown((dropdown: DropdownComponent) => {
+					dropdown
+						.addOption(OpenCreatedNotebook.DONT, 'Do not open')
+						.addOption(
+							OpenCreatedNotebook.CURRENT_TAB,
+							'Open in the current tab (default)'
+						)
+						.addOption(OpenCreatedNotebook.NEW_TAB, 'Open in a new tab')
+						.addOption(OpenCreatedNotebook.SPLIT, 'Open in a new split tab')
+						.addOption(OpenCreatedNotebook.WINDOW, 'Open in a detached window')
+						.setValue(plugin.settings.openCreatedFileMode)
+						.onChange((value: OpenCreatedNotebook) => {
+							plugin.settings.openCreatedFileMode = value;
+						});
+				});
+		}
+	});
 }
--- src/services/jupyter-modal.ts
+++ src/services/jupyter-modal.ts
@@ -1,10 +1,10 @@
-import { App, ButtonComponent, Modal, Setting } from "obsidian";
+import { App, ButtonComponent, Modal, Setting } from 'obsidian';
 
 /** Contract to define what a button should do and look like. */
 export interface JupyterModalButton {
-    text: string;
-    onClick: () => void;
-    closeOnClick: boolean;
+	text: string;
+	onClick: () => void;
+	closeOnClick: boolean;
 }
 
 /**
@@ -12,33 +12,37 @@ export interface JupyterModalButton {
  * a title, a message and a set of buttons.
  */
 export class JupyterModal extends Modal {
-    constructor(
-        app: App,
-        private readonly heading: string,
-        private readonly message: string[],
-        private readonly buttons: JupyterModalButton[]
-    ) {
-        super(app);
-    }
+	constructor(
+		app: App,
+		private readonly heading: string,
+		private readonly message: string[],
+		private readonly buttons: JupyterModalButton[]
+	) {
+		super(app);
+	}
 
-    onOpen(): void {
-        this.contentEl.createEl("h1", { text: this.heading });
-        for (const message of this.message) {
-            this.contentEl.createEl("p", { text: message });
-        }
-        if (this.buttons.length > 0) {
-            let setting = new Setting(this.contentEl);
-            this.buttons.forEach(((button: JupyterModalButton) => {
-                setting.addButton(((buttonEl: ButtonComponent) => {
-                    buttonEl.setButtonText(button.text);
-                    buttonEl.onClick(() => {
-                        button.onClick();
-                        if (button.closeOnClick) {
-                            this.close();
-                        }
-                    });
-                }).bind(this));
-            }).bind(this));
-        }
-    }
+	onOpen(): void {
+		this.contentEl.createEl('h1', { text: this.heading });
+		for (const message of this.message) {
+			this.contentEl.createEl('p', { text: message });
+		}
+		if (this.buttons.length > 0) {
+			let setting = new Setting(this.contentEl);
+			this.buttons.forEach(
+				((button: JupyterModalButton) => {
+					setting.addButton(
+						((buttonEl: ButtonComponent) => {
+							buttonEl.setButtonText(button.text);
+							buttonEl.onClick(() => {
+								button.onClick();
+								if (button.closeOnClick) {
+									this.close();
+								}
+							});
+						}).bind(this)
+					);
+				}).bind(this)
+			);
+		}
+	}
 }
